d66b035919ceea6382e5f7a97ae4de8650798f40
Andy Fundinger <Andy@fundinger.name>
2013-06-01 09:26:18 -0400

    added comments small renaming

--- flock/lambdas.py
+++ flock/lambdas.py
@@ -1,13 +1,29 @@
 __author__ = 'andriod'
 
 
-def lookup(character, attribute, table):
-    return lambda: table.get(character[attribute], None)
+def lookup(mapping, index, table):
+    """
+    Create a lambda that looks up the specified value from the table
+    :type mapping: Mapping the FlockDict or other Mapping to look up from
+    :type index: str the index in the Mapping to check
+    :type table: Mapping the table to lookup from
+    :return:the 0 parameter lambada-closure that returns value as found in the table
+    """
+    return lambda: table.get(mapping[index], None)
 
 
-def reference(obj, *indexes):
+def reference(flock, *indexes):
+    """
+    return closure that references values stored elsewhere in the Flock
+    :type flock: flock.core.FlockDict
+    :param indexes: lambdas to be resolved in order
+    :return: 0 parameter function with all parameters included as a closure, returns referenced value
+    """
+
     def de_ref():
-        currObj = obj
+        currObj = flock
+
+        # recursively resolve indexes
         for index in indexes:
             currObj = currObj[index]
         return currObj
