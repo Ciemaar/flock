d2a5aea3ff6dcb2d42093110ada2834441385a4c
Andy Fundinger <Andy@Fundinger.name>
2016-05-30 17:45:23 -0400

    Basic Setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='flock',
+    version='0.0.1',
+    packages=['test', 'flock'],
+    url='http://ciemaar.com/flock',
+    license='GPL v3',
+    author='Andy Fundiger',
+    author_email='Andy@ciemaar.com',
+    description=''
+)
