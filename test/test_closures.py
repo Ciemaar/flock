fcad80ab450ddd58d773a95d7189bdaea243efab
Andy Fundinger <Andy@Fundinger.name>
2016-05-31 12:53:42 -0400

    added test_lookup

--- test/test_closures.py
+++ test/test_closures.py
@@ -1,15 +1,23 @@
 import unittest
 import uuid as uuid
 
-from flock.closures import reference
+from flock.closures import reference, lookup
 
 
-class MyTestCase(unittest.TestCase):
+class ClosureTestCase(unittest.TestCase):
 
     def test_reference(self):
         probe = uuid.uuid4()
         base_dict={'x':probe}
         assert probe is reference(base_dict,'x')()
+
+    def test_lookup(self):
+        probe = uuid.uuid4()
+        table_row = 5
+        base_dict = {'x': table_row}
+        table = {table_row: probe}
+        assert probe is lookup(base_dict, 'x', table)()
+
 
 
 if __name__ == '__main__':
