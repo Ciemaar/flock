0ce5bb68ac46ca33c013118290b055fe73d3b740
Andy Fundinger <Andy@Fundinger.name>
2016-11-13 01:06:37 -0500

    Fixed spelling of my name.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['test', 'flock'],
     url='http://ciemaar.com/flock',
     license='GPL v3',
-    author='Andy Fundiger',
+    author='Andy Fundinger',
     author_email='Andy@ciemaar.com',
     description=''
 )
