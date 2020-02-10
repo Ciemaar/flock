5133c4bb35a2cfff5b883a8fd6b19e1179d9dccb
Andy Fundinger <Andy@ciemaar.com>
2020-02-10 11:29:36 -0500

    added simulate characters

---
+++ examples/simulate_characters.py
@@ -0,0 +1,15 @@
+import mythica.model
+
+def die(num, sides, rnd=None):
+    if not rnd:
+        rnd = random
+    return sum(rnd.randint(1, sides) for x in range(num))
+
+def model():
+    char = FlockDict(sheet)
+    mythica.model.apply_rules(char)
+    char['rand'] = lambda: random.Random(char['seed'])
+    char['roll'] = lambda: partial(die, rnd=char['rand'])
+    char['rolls'] = lambda: [char['roll'](1,10) + char['roll'](1,10) for _ in range(12)]
+    char['sorted_rolls'] = lambda: sorted(char['rolls'])
+    return char
