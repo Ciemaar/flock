3e3fa31830c6911c1e9a9fb54de2fb07a85fd91a
Andy Fundinger <andy@ciemaar.com>
2022-05-30 08:52:04 -0400

    Merge from Master

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='flock',
-    version='0.0.11',
+    name='closure_collector',
+    version='0.0.1',
     packages=['test', 'flock'],
     url='http://ciemaar.com/flock',
     license='GPL v3',
     author='Andy Fundinger',
     author_email='Andy@ciemaar.com',
-    description='',
+    description='A library for storing closures inside objects in an organized fashion.',
     requires=['PyYAML', 'wheel', 'pytest']
 )
