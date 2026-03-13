26d7010f698d7941cf7da028b74ab521db9ed719
Andy Fundinger <andy@ciemaar.com>
2026-03-13 05:14:52 -0400

    Ruff formatted.

--- examples/simulate_characters.py
+++ examples/simulate_characters.py
@@ -1,13 +1,23 @@
+"""EXPERIMENTAL simulate characters"""
+
+import random
+from functools import partial
+
 import mythica.model
+from flock.core import FlockDict
+
+sheet = {"seed": 42}
 
 
 def die(num, sides, rnd=None):
+    """Local die function"""
     if not rnd:
         rnd = random
     return sum(rnd.randint(1, sides) for x in range(num))
 
 
 def model():
+    """create and return a character"""
     char = FlockDict(sheet)
     mythica.model.apply_rules(char)
     char["rand"] = lambda: random.Random(char["seed"])
